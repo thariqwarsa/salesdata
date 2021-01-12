67c1ac5b4131466ee676f7da1d8c552aa955c9e6
thariq_w <thariqwarsahemas@gmail.com>
2021-01-12 15:25:12 +0700

    add crude date form

--- src/component/Date.js
+++ src/component/Date.js
@@ -1,10 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import rangeDate from '../data/year-end.json';
 
 export class Date extends Component {
+
+
   render() {
+    const renderDate = rangeDate.data.range_date.map(
+      date => { return (<option>{date}</option>) }
+    );
+
     return (
-      <div>
-        Date component
+      <div className='Date'>
+        <div class="form-group">
+          <label htmlFor="from">From</label>
+          <select className="form-control" name='from-date' id="from">
+            {renderDate}
+          </select>
+        </div>
+
+        <div class="form-group">
+          <label htmlFor="to">To</label>
+          <select className="form-control" name='to-date' id="to">
+            {renderDate}
+          </select>
+        </div>
       </div>
     )
   }
