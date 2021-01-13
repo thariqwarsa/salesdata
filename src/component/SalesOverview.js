711e4263f065ecd8833f7fd4b40e1edac932db98
thariq_w <thariqwarsahemas@gmail.com>
2021-01-13 20:34:38 +0700

    style sales-overview, patch with data from json

--- src/component/SalesOverview.js
+++ src/component/SalesOverview.js
@@ -1,13 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import overviewData from '../data/sales-overview.json'
 
 export class SalesOverview extends Component {
   render() {
+    const { order_cancel, order_paid, order_pending, today_sales,
+      total_cancel, total_pending, total_shipping } = overviewData.data
+
     return (
       <div className='SalesOverview'>
-        Sales Overview!
+        <div className='container'>
+
+          <div className="row">
+            <div className="col-sm-4">
+              <div className="card paid">
+                <div className="card-body">
+                  <p className="card-text">Sales <span>{order_paid} Paid Order</span></p>
+                  <h5 className="card-title">Rp. {today_sales.toLocaleString(['ban', 'id'])}</h5>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-sm-4">
+              <div className="card cancel">
+                <div className="card-body">
+                  <p className="card-text">Cancel <span>{order_cancel} Orders</span></p>
+                  <h5 className="card-title">Rp. {total_cancel.toLocaleString(['ban', 'id'])}</h5>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-sm-4">
+              <div className="card pending">
+                <div className="card-body">
+                  <p className="card-text">Pending <span>{order_pending} Orders</span></p>
+                  <h5 className="card-title">Rp. {total_pending.toLocaleString(['ban', 'id'])}</h5>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
       </div>
     )
   }
 }
 
 export default SalesOverview
+
