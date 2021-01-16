ef3ba08335440c8de101b549c4cce8023b839884
thariq_w <thariqwarsahemas@gmail.com>
2021-01-16 16:47:41 +0700

    add crosshair but still failed

--- src/component/MultipleLineChart.js
+++ src/component/MultipleLineChart.js
@@ -3,23 +3,32 @@ import { data } from '../data/year-end.json';
 
 // import react-vis
 import '../../node_modules/react-vis/dist/style.css';
-import { XYPlot, LineSeries, XAxis, YAxis, HorizontalGridLines, DiscreteColorLegend } from 'react-vis';
+import {
+  XYPlot, LineSeries, XAxis, YAxis, HorizontalGridLines,
+  DiscreteColorLegend, Crosshair
+} from 'react-vis';
 
 export class MultipleLineChart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { crosshairValue: [] }
+    this._onMouseLeave = this._onMouseLeave.bind(this);
+    this._onNearestX = this._onNearestX.bind(this);
+  }
 
   filterData(start, end) {
     const datas = Object.values(data.metric.clicks);
     let grab = false;
     let allData = [];
     let clientData = {};
-    // let clientNum = datas.length;
+    let clientNum = 4
 
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < clientNum; i++) {
       clientData = { 'account_name': datas[i].account_name, 'data': [] }
       grab = false;
       for (let j = 0; j < datas[i].data.length; j++) {
         if (datas[i].data[j][0] === start) grab = true;
-        if (grab) clientData.data.push({ x: datas[i].data[j][0].slice(0, 6), y: datas[i].data[j][1] })
+        if (grab) clientData.data.push({ x: datas[i].data[j][0].slice(0, 6), y: datas[i].data[j][1], account_name: datas[i].account_name })
         if (datas[i].data[j][0] === end) break;
       }
       allData.push(clientData);
@@ -27,18 +36,25 @@ export class MultipleLineChart extends Component {
     return allData;
   }
 
+  _onMouseLeave() {
+    this.setState({ crosshairValues: [] });
+  }
+
+  _onNearestX(value, { index }) {
+    this.setState({ crosshairValues: [] });
+  }
+
   render() {
     const datas = this.filterData(this.props.startDate, this.props.endDate);
     const account_names = datas.map(d => d.account_name);
-
+    const { crosshairValue } = this.state;
     return (
-      <div className="MultipleLineChart">
+      < div className="MultipleLineChart" >
         <XYPlot
           xType="ordinal"
           className='MultipleLineChart'
           width={600}
           height={200}
-
         >
           <XAxis
             tickFormat={(t, i) => {
@@ -52,20 +68,33 @@ export class MultipleLineChart extends Component {
           <HorizontalGridLines />
           {
             datas.length && datas.map(d => {
-              return (<LineSeries key={d.account_name} data={d.data} curve={'curveMonotoneX'} />)
+              return (
+                <LineSeries
+                  key={d.account_name}
+                  data={d.data}
+                  curve={'curveMonotoneX'}
+                  onNearestX={this._onNearestX}
+                  onMouseLeave={this._onMouseLeave}
+                />
+              )
             })
           }
+          {/* // {crosshairValue &&
+          //   <Crosshair values={[crosshairValue]}>
+            //     <div style={{ backgroundColor: 'white', border: '1px solid black', width: '12em' }}>
+              //       <div>{crosshairValue.x}</div>
+          //     </div>
+          //   </Crosshair>
+          // } */}
         </XYPlot>
         <div className='legend'>
-          <DiscreteColorLegend items={account_names}
-            orientation='horizontal' width={600} />
-          {/* <div className='legend'>
-          <DiscreteColorLegend items={account_names.slice(0, 8)}
-            orientation='horizontal' width={600} />
-          <DiscreteColorLegend items={account_names.slice(8)}
-            orientation='horizontal' width={600} />*/}
+          <DiscreteColorLegend
+            items={account_names}
+            orientation='horizontal'
+            width={600}
+          />
         </div>
-      </div>
+      </div >
     )
   }
 }
